86123b2a2c11b5968439d9451b437f0928deef70
Bryan Tran <bryant.business@hotmail.com>
2022-04-13 00:35:54 -0700

    takeoutserver.js

--- server.js
+++ server.js
@@ -9,71 +9,71 @@ app.listen(process.env.PORT || 5000, function (err) {
 
 app.use(express.static("public"))
 
-const bodyparser = require("body-parser");
-app.use(bodyparser.urlencoded({
-    extended: true
-}));
+//const bodyparser = require("body-parser");
+//app.use(bodyparser.urlencoded({
+//    extended: true
+//}));
 
-
-const mongoose = require('mongoose');
+//
+//const mongoose = require('mongoose');
 
 // mongoose.connect("mongodb+srv://nabil828:comp1537@cluster0.lbm8g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
 //     { useNewUrlParser: true, useUnifiedTopology: true });
 
-mongoose.connect('mongodb+srv://LoveSongBB:123@1537-bryantranvan.fdv5r.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true});
-const unicornSchema = new mongoose.Schema({
-    name: String,
-    weight: Number,
-    loves: [String]
-});
-const unicornModel = mongoose.model("unicorns", unicornSchema);
-
-
-
-app.post("/findUnicornByName", function (req, res) {
-    console.log("req. has been received")
-    console.log(req.body.unicornName)
-
-    unicornModel.find({ name: req.body.unicornName }, function (err, unicorns) {
-        if (err) {
-            console.log("Error " + err);
-        } else {
-            console.log("Data " + unicorns);
-        }
-        res.send(unicorns);
-    });
-})
-
-
-
-
-app.post("/findUnicornByFood", function (req, res) {
-    console.log("req. has been received")
-    console.log(req.body.appleIsChecked)
-    console.log(req.body.carrotIsChecked)
-    aList = []
-    if (req.body.appleIsChecked == "checked")
-        aList.push("apple")
-
-
-    if (req.body.carrotIsChecked == "checked")
-        aList.push("carrot")
-
-    unicornModel.find({
-        loves: {
-            $in: aList
-        }
-    }, function (err, unicorns) {
-        if (err) {
-            console.log("Error " + err);
-        } else {
-            console.log("Data " + unicorns);
-        }
-        res.send(unicorns);
-    });
-
-
-})
+//mongoose.connect('mongodb+srv://LoveSongBB:123@1537-bryantranvan.fdv5r.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true});
+//const unicornSchema = new mongoose.Schema({
+//    name: String,
+//    weight: Number,
+//    loves: [String]
+//});
+//const unicornModel = mongoose.model("unicorns", unicornSchema);
+//
+//
+//
+//app.post("/findUnicornByName", function (req, res) {
+//    console.log("req. has been received")
+//    console.log(req.body.unicornName)
+//
+//    unicornModel.find({ name: req.body.unicornName }, function (err, unicorns) {
+//        if (err) {
+//            console.log("Error " + err);
+//        } else {
+//            console.log("Data " + unicorns);
+//        }
+//        res.send(unicorns);
+//    });
+//})
+//
+//
+//
+//
+//app.post("/findUnicornByFood", function (req, res) {
+//    console.log("req. has been received")
+//    console.log(req.body.appleIsChecked)
+//    console.log(req.body.carrotIsChecked)
+//    aList = []
+//    if (req.body.appleIsChecked == "checked")
+//        aList.push("apple")
+//
+//
+//    if (req.body.carrotIsChecked == "checked")
+//        aList.push("carrot")
+//
+//    unicornModel.find({
+//        loves: {
+//            $in: aList
+//        }
+//    }, function (err, unicorns) {
+//        if (err) {
+//            console.log("Error " + err);
+//        } else {
+//            console.log("Data " + unicorns);
+//        }
+//        res.send(unicorns);
+//    });
+//
+//
+//})
 
 
 //app.get('/', function (req, res) {
